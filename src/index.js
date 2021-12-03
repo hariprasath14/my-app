b291836086f654d87f5afd77e0331bffb6eee146
hari prasath <hariprasath.selvaraj@lystloc.com>
2021-12-04 02:04:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+{console.log(":::::",process.env) }
   </React.StrictMode>,
   document.getElementById('root')
 );
