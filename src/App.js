bfe967538639151862800e8ebdd257354198eec7
hari prasath <hariprasath.selvaraj@lystloc.com>
2022-03-13 22:32:12 +0530

    profile update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import myImage from './images/me.jpg'
+import './myStyle.scss'
+import './genaral.scss'
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav/>
+      <div className='d-flex profile-container'>
+        <img src={myImage} className="profile-pic" />
+        <div class="waviy">
+          <span style={{"--i":"1"}}>H</span>
+          <span style={{"--i":"2"}}>A</span>
+          <span style={{"--i":"3"}}>R</span>
+          <span style={{"--i":"4"}}>I</span>
+          <span style={{"--i":"5"}}>&nbsp;</span>
+          <span style={{"--i":"6"}}>P</span>
+          <span style={{"--i":"7"}}>R</span>
+          <span style={{"--i":"8"}}>A</span>
+          <span style={{"--i":"9"}}>S</span>
+          <span style={{"--i":"10"}}>A</span>
+          <span style={{"--i":"11"}}>T</span>
+          <span style={{"--i":"12"}}>H</span>
+        </div>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Page
+          </p>
+          <a
+            className="App-link"
+            href="#"
+            rel="noopener noreferrer"
+          >
+            In Construction
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
