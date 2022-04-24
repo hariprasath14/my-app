db8a425f5ae01a7f1ffc60e0c0fc754b4c90fd85
hari prasath <hariprasath.selvaraj@lystloc.com>
2022-04-24 23:26:50 +0530

    alignment cleared

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect,useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../../App.css';
 import '../../scss/home.scss';
 import myImage from '../../assets/images/me.jpg'
@@ -9,56 +9,56 @@ import '../../scss/genaral.scss'
 import Nav from '../navbar/Nav';
 
 const useAudio = url => {
-    const [audio] = useState(new Audio(url));
-    const [playing, setPlaying] = useState(false);
-    const toggle = () => setPlaying(!playing);
-  
-    useEffect(() => {
-        playing ? audio.play() : audio.pause();
-      },
-      [playing]
-    );
-  
-    useEffect(() => {
-      audio.addEventListener('ended', () => setPlaying(false));
-      return () => {
-        audio.removeEventListener('ended', () => setPlaying(false));
-      };
-    }, []);
-  
-    return [playing, toggle];
-  };
-  
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
 
 function Home() {
-    
-    const [audio] = useState(new Audio(gunFireMp3));
-    const [johnyBGM] = useState(new Audio(johnyMp3));
-    const [thunderBGM] = useState(new Audio(thunderMp3));
-    const [playjohny,setPlayJohny] = useState(false);
-    const [hideRain, sethideRain] = useState(false);
-    const [gunFire,setgunFire] = useState(false);
-    const [frontRow,setfrontRow] = useState([]);
-    const [backRow,setbackRow] = useState([]);
-    const [thunder, setThunder] = useState(false);
-    useEffect(() => {
-      gunFire?audio.play():audio.pause()
-      },
-      [gunFire] 
-    )
-    useEffect(() => {
-        audio.addEventListener('ended', () => setgunFire(false));
-        return () => {
-            audio.removeEventListener('ended', () => setgunFire(false));
-        };
-    }, []);
-    useEffect(() => {
-      playjohny?thunderBGM.play():thunderBGM.pause()
-      setTimeout(() => {
-        playjohny?johnyBGM.play():johnyBGM.pause()
-      }, 3000);
-    },
-    [playjohny] 
+
+  const [audio] = useState(new Audio(gunFireMp3));
+  const [johnyBGM] = useState(new Audio(johnyMp3));
+  const [thunderBGM] = useState(new Audio(thunderMp3));
+  const [playjohny, setPlayJohny] = useState(false);
+  const [hideRain, sethideRain] = useState(false);
+  const [gunFire, setgunFire] = useState(false);
+  const [frontRow, setfrontRow] = useState([]);
+  const [backRow, setbackRow] = useState([]);
+  const [thunder, setThunder] = useState(false);
+  useEffect(() => {
+    gunFire ? audio.play() : audio.pause()
+  },
+    [gunFire]
+  )
+  useEffect(() => {
+    audio.addEventListener('ended', () => setgunFire(false));
+    return () => {
+      audio.removeEventListener('ended', () => setgunFire(false));
+    };
+  }, []);
+  useEffect(() => {
+    playjohny ? thunderBGM.play() : thunderBGM.pause()
+    setTimeout(() => {
+      playjohny ? johnyBGM.play() : johnyBGM.pause()
+    }, 3000);
+  },
+    [playjohny]
   )
   // useEffect(() => {
   //     playjohny.addEventListener('ended', () => setgunFire(false));
@@ -67,97 +67,97 @@ function Home() {
   //     };
   // }, []);
 
-  const fetchOnScroll=(e)=>{
-   if(e.target.scrollTop>100){
-    sethideRain(true)
-   }else{
-    sethideRain(false)
-   }
+  const fetchOnScroll = (e) => {
+    if (e.target.scrollTop > 100) {
+      sethideRain(true)
+    } else {
+      sethideRain(false)
+    }
   }
-    const makeThunder=()=>{
+  const makeThunder = () => {
+    setTimeout(() => {
+      makeItRain();
+      makeItRain();
+      makeItRain();
+      makeItRain();
+      makeItRain();
       setTimeout(() => {
         makeItRain();
         makeItRain();
         makeItRain();
-        makeItRain();
-        makeItRain();
-        setTimeout(() => {
-        makeItRain();
-        makeItRain();
-        makeItRain();
-        }, 1000);
-        setThunder(true)
-      }, 5000);
-    }
-    const makeItRain =()=> {
-      if(frontRow?.length<200){
-        var increment = 0;
-        while(increment<100){
-            let randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
-            //random number between 5 and 2
-            let randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
-            //increment
-            let incrementValue = increment+randoFiver;
-            increment = incrementValue;
-  
-            setfrontRow(prevfrontRow=>[...prevfrontRow,<div className="drop" style={{left: `${incrementValue}%`, bottom: `${randoFiver + randoFiver - 1 + 100}%`, animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`,}}>
-              <div className="stem" style={{animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`}}></div>
-              <div className="splat" style={{animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`}}></div>
-            </div>])            
-            setbackRow(prevbackRow=>[...prevbackRow,<div className="drop" style={{right:`${incrementValue}%`, bottom: `${randoFiver + randoFiver - 1 + 100}%`,  animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`,}}>
-              <div className="stem" style={{animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`}}></div>
-              <div className="splat" style={{animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`}}></div>
-            </div>])
-        }
+      }, 1000);
+      setThunder(true)
+    }, 5000);
+  }
+  const makeItRain = () => {
+    if (frontRow?.length < 200) {
+      var increment = 0;
+      while (increment < 100) {
+        let randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+        //random number between 5 and 2
+        let randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+        //increment
+        let incrementValue = increment + randoFiver;
+        increment = incrementValue;
+
+        setfrontRow(prevfrontRow => [...prevfrontRow, <div className="drop" style={{ left: `${incrementValue}%`, bottom: `${randoFiver + randoFiver - 1 + 100}%`, animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`, }}>
+          <div className="stem" style={{ animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s` }}></div>
+          <div className="splat" style={{ animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s` }}></div>
+        </div>])
+        setbackRow(prevbackRow => [...prevbackRow, <div className="drop" style={{ right: `${incrementValue}%`, bottom: `${randoFiver + randoFiver - 1 + 100}%`, animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s`, }}>
+          <div className="stem" style={{ animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s` }}></div>
+          <div className="splat" style={{ animationDelay: `0.${randoHundo}s`, animationDuration: `0.5${randoHundo}s` }}></div>
+        </div>])
       }
     }
+  }
   return (
-    <div className='home-container' 
-    onScroll={(e)=>{fetchOnScroll(e);}}
+    <div className='home-container'
+      onScroll={(e) => { fetchOnScroll(e); }}
     >
       {/* <Nav/> */}
       <div className='d-flex profile-container rain-flow back-row-toggle splat-toggle additional'>
-        <img src={myImage} className={`profile-pic ${frontRow?.length>0?"filter-profile":""}  ${thunder?"thunder":""}`} />
-        <div  className={`waviy ${frontRow?.length>0?"opacity-100 visible":""}`}>
-          <span style={{"--i":"1"}}>H</span>
-          <span style={{"--i":"2"}}>A</span>
-          <span style={{"--i":"3"}}>R</span>
-          <span style={{"--i":"4"}}>I</span>
-          <span style={{"--i":"5"}}>&nbsp;</span>
-          <span style={{"--i":"6"}}>P</span>
-          <span style={{"--i":"7"}}>R</span>
-          <span style={{"--i":"8"}}>A</span>
-          <span style={{"--i":"9"}}>S</span>
-          <span style={{"--i":"10"}}>A</span>
-          <span style={{"--i":"11"}}>T</span>
-          <span style={{"--i":"12"}}>H</span>
+        <img src={myImage} className={`profile-pic ${frontRow?.length > 0 ? "filter-profile" : ""}  ${thunder ? "thunder" : ""}`} />
+        <div className={`waviy ${frontRow?.length > 0 ? "opacity-100 visible" : ""}`}>
+          <span style={{ "--i": "1" }}>H</span>
+          <span style={{ "--i": "2" }}>A</span>
+          <span style={{ "--i": "3" }}>R</span>
+          <span style={{ "--i": "4" }}>I</span>
+          <span style={{ "--i": "5" }}>&nbsp;</span>
+          <span style={{ "--i": "6" }}>P</span>
+          <span style={{ "--i": "7" }}>R</span>
+          <span style={{ "--i": "8" }}>A</span>
+          <span style={{ "--i": "9" }}>S</span>
+          <span style={{ "--i": "10" }}>A</span>
+          <span style={{ "--i": "11" }}>T</span>
+          <span style={{ "--i": "12" }}>H</span>
         </div>
-        <div className={`rain front-row ${hideRain?"hide-spalt":""}`}>
-                {frontRow.map((drop)=>{
-                  return drop
-                })}
-            </div>
-            <div className={`rain back-row ${hideRain?"hide-spalt":""}`}>
-                {backRow.map((drop)=>{
-                  return drop
-                })}
-            </div>
-          <div className={`play-rain ${frontRow?.length>0?"opacity-0 invisible ":""}`}  onClick={(e)=>{makeItRain();makeThunder();setPlayJohny(true)}}>
-              Play
-          </div> 
+        <div className={`rain front-row ${hideRain ? "hide-spalt" : ""}`}>
+          {frontRow.map((drop) => {
+            return drop
+          })}
+        </div>
+        <div className={`rain back-row ${hideRain ? "hide-spalt" : ""}`}>
+          {backRow.map((drop) => {
+            return drop
+          })}
+        </div>
+        <div className={`play-rain ${frontRow?.length > 0 ? "opacity-0 invisible " : ""}`} onClick={(e) => { makeItRain(); makeThunder(); setPlayJohny(true) }}>
+          Play
+        </div>
       </div>
       <div className="App">
         <header className="App-header">
-            
+
           {/* <p className='cursor-pointer px-4' onClick={(e)=>{console.log(frontRow, backRow);}}>Play</p> */}
-            
+
           <p>
             Page
           </p>
           <a
             href="#"
             rel="noopener noreferrer"
-            onClick={()=>setgunFire(!gunFire)}
+            onClick={() => setgunFire(!gunFire)}
             className="App-link mt-3 cursor-pointer">
             In Construction
           </a>
